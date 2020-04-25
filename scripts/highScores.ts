b28310b9a4985449f118972544a81638f3280b05
Canaan Epperson <cepperson16@georgefox.edu>
2020-04-24 18:04:51 -0700

    working on high score database

--- scripts/highScores.ts
+++ scripts/highScores.ts
@@ -1,9 +1,18 @@
 
 function getHighScores(id) {
-    $.get("https://9o9edf83h4.execute-api.us-west-2.amazonaws.com/catFactsHighScores",
-        function (data) {
+
+    $.ajax({
+        url: "https://9o9edf83h4.execute-api.us-west-2.amazonaws.com/catFactsHighScores",
+        accepts: {
+            "Access-Control-Allow-Origin": "https://disciple153.github.io/CSIS304-FinalProject/"
+        },
+        success: function (data) {
             console.log(JSON.stringify(data));
-        });
+        },
+        error: function (x, y, z) {
+            console.log("ERROR\n" + JSON.stringify(x) + "\n" + y + "\n" + z);
+        }
+    });
 }
 
 function submitHighScore(id) {
